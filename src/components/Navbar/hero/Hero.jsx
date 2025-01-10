5e4355415b21e5794286a04f74f88cacc1d623f1
notaravinth <aravinthravichandran2004@gmail.com>
2025-01-10 23:43:57 +0530

    picture changes

--- src/components/Navbar/hero/Hero.jsx
+++ src/components/Navbar/hero/Hero.jsx
@@ -1,10 +1,15 @@
 import React from 'react'
 import './hero.css'
+import dark_arrow from '../../../assets/dark_arrow.png'
 
 const hero = () => {
   return (
-    <div className='hero'>
-      
+    <div className='hero container'>
+      <div className="hero-text">
+        <h1>We ensure better education for a better world</h1>
+        <p>Our cutting-edge curriculum is designed to empower students with the knowledge, skills and experiences needed to excel in the dynamic field of education</p>
+        <button className='btn'>Explore More <img src={dark_arrow} alt="" /></button>
+      </div>
     </div>
   )
 }
