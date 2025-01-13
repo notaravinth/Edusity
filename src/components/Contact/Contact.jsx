3e54ca17adf3fb47f8030cb4d066dda09d62dbc8
notaravinth <aravinthravichandran2004@gmail.com>
2025-01-13 20:38:19 +0530

    contact

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
 import './contact.css'
 import message_icon from '../../assets/msg-icon.png'
+import mail_icon from '../../assets/mail-icon.png'
+import phone_icon from '../../assets/phone-icon.png'
+import location_icon from '../../assets/location-icon.png'
+import white_arrow from '../../assets/white-arrow.png'
 
 const Contact = () => {
   return (
@@ -9,10 +13,22 @@ const Contact = () => {
         <h3>Send us a message <img src={message_icon} alt="" /></h3>
         <p>Feel free to reach out to us through the contact form. your feedback is important to us as we strive to provide exceptional service to our community.</p>
         <ul>
-            <li>Contact@aravinth.dev</li>
-            <li>+91 1234567890</li>
-            <li>somewhere on planet earth</li>
+            <li><img src={mail_icon} alt="" className=''/>Contact@aravinth.dev</li>
+            <li><img src={phone_icon} alt="" />+91 1234567890</li>
+            <li><img src={location_icon} alt="" />somewhere on planet earth</li>
         </ul>
+      </div>
+      <div className="contact-col">
+        <form>
+          <label>Your Name</label>
+          <input type="text" name="name" placeholder="Enter your name" required/>
+          <label>Phone Number</label>
+          <input type="tel" name="phone" placeholder="Enter your mobile number" required/>
+          <label>Your Name</label>
+          <textarea name="message" rows="6" placeholder='Enter your message' required></textarea>
+          <button type='submit' className='btn dark-btn'>submit now <img src={white_arrow} alt="" /></button>
+        </form>
+        <span></span>
       </div>
     </div>
   )
