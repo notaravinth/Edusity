9973514237be455a04498dafbe06ff7f88c32261
notaravinth <aravinthravichandran2004@gmail.com>
2025-01-15 20:47:47 +0530

    hiding menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import { Link } from 'react-scroll';
 import './navbar.css';
 import logo from '../../assets/logo.png';
+import menu_icon from '../../assets/menu-icon.png';
 
 const Navbar = () => {
   const [sticky, setSticky] = useState(false);
@@ -17,10 +18,16 @@ const Navbar = () => {
     };
   }, []);
 
+
+  const {mobileMenu, setMobileMenu} = useState(false);
+  const toggleMenu = () => {
+    mobileMenu ? setMobileMenu(false) : setMobileMenu(true);
+  }
+
   return (
     <nav className={`navbar-container ${sticky ? 'dark-nav' : ''}`}>
       <img src={logo} alt="Logo" className="logo" />
-      <ul>
+      <ul className={mobileMenu?'':'hide-mobile-menu'}>
         <li><Link to="hero" smooth={true} offset={0} duration={500}>Home</Link></li>
         <li><Link to="program" smooth={true} offset={-260} duration={500}>Program</Link></li>
         <li><Link to="about" smooth={true} offset={-150} duration={500}>About Us</Link></li>
@@ -28,6 +35,7 @@ const Navbar = () => {
         <li><Link to="testimonials" smooth={true} offset={-260} duration={500}>Testimonials</Link></li>
         <li><Link to="contact" smooth={true} offset={-260} duration={500} className="btn">Contact Us</Link></li>
       </ul>
+      <img src={menu_icon} alt=""  className='menu-icon' onClick={toggleMenu}/>
     </nav>
   );
 };
