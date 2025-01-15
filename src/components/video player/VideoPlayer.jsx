d391fff0fd790de099832ebe54055cc51774769a
notaravinth <aravinthravichandran2004@gmail.com>
2025-01-15 22:41:54 +0530

    completed

---
+++ src/components/video player/VideoPlayer.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './VideoPlayer.css'
+
+const VideoPlayer = () => {
+  return (
+    <div className='video-Player'>
+      <video src=""></video>
+    </div>
+  )
+}
+
+export default VideoPlayer
